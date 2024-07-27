c7d54fa051891357c7f1abd3720c0875dc740902
Tushar Gautam <tushargautam@Tushars-MacBook-Air.local>
2024-07-27 13:27:20 +0530

    msw initial setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,31 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    let isMounted = true;
+
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:5173/user");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const user = await response.json();
+        if (isMounted) {
+          console.log(user);
+        }
+      } catch (error) {
+        console.error("Fetch error: ", error);
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <>
       <div>Hello Test</div>
