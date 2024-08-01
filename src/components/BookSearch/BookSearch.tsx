fe9d61a6f852204b20d12dbad78357764e3779a5
Tushar Gautam <tushargautam@Tushars-MacBook-Air.local>
2024-08-01 11:07:45 +0530

    added debounce

--- src/components/BookSearch/BookSearch.tsx
+++ src/components/BookSearch/BookSearch.tsx
@@ -1,4 +1,12 @@
-import { ChangeEvent, Dispatch, FC, useState } from "react";
+import {
+  ChangeEvent,
+  Dispatch,
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { debounce } from "lodash";
 import "./style.css";
 import useFetchBooks from "../../hooks/useFetchBooks";
 
@@ -18,15 +26,30 @@ const BookSearch: FC<BookSearchProps> = ({
   selectedBooks,
   setSelectedBooks,
 }) => {
+  const [inputValue, setInputValue] = useState("");
   const [query, setQuery] = useState("");
   const { results, isLoading } = useFetchBooks(query);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetQuery = useCallback(
+    debounce((value: string) => setQuery(value), 500),
+    []
+  );
+
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+    setInputValue(e.target.value);
+    debouncedSetQuery(e.target.value);
   };
+
+  useEffect(() => {
+    return () => {
+      debouncedSetQuery.cancel();
+    };
+  }, [debouncedSetQuery]);
 
   const handleResultClick = (book: Book) => {
     setSelectedBooks([...selectedBooks, book]);
+    setInputValue("");
     setQuery("");
   };
 
@@ -35,7 +58,7 @@ const BookSearch: FC<BookSearchProps> = ({
       <div className="book-search">
         <input
           type="text"
-          value={query}
+          value={inputValue}
           onChange={handleInputChange}
           placeholder="Search for a book summary..."
         />
@@ -44,7 +67,7 @@ const BookSearch: FC<BookSearchProps> = ({
           <ul className="results-list">
             {results.map((book) => (
               <li key={book.id} onClick={() => handleResultClick(book)}>
-                {book.id}&#41; {book.title}
+                {book.id}) {book.title}
               </li>
             ))}
           </ul>
